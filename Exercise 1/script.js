db19a259d19c8c36c2cb1699a4005c6bf3a8a4cf
dennisvantonder <dennisvantonder@outlook.com>
2022-08-05 13:37:59 +0200

    Initial commit

---
+++ Exercise 1/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// How JavaScript changed my perspective of IT.
+
+// Different approaches on how to use funtions in JavaScript
+
+const heading = document.querySelector("h1");
+const h2 = document.querySelector("h2");
+const h3 = document.querySelector("h3");
+// Common method:
+function reverseString(someText) {
+  let reverse = "";
+  for (let i = someText.length - 1; i >= 0; i--) {
+    reverse += someText[i];
+  }
+  return reverse;
+}
+
+// With function Expressions:
+// Function expressions let you define functions as variables
+const isPalindrome = function (someText) {
+  console.log(reverseString(someText));
+  if (reverseString(someText) == someText) {
+    h3.textContent = `The string ${someText} is a palindrome!`;
+  } else {
+    h3.textContent = `The string ${someText} is not a palindrome!`;
+  }
+};
+
+// With Arrow functions;
+document
+  .querySelector("body")
+  .addEventListener("click", () => (h3.textContent = "JavaScript is fun!"));
+
+const string = prompt("Enter any string: ");
+h2.textContent = `The reverse of ${string} is: ${reverseString(string)}`;
+isPalindrome(string);
